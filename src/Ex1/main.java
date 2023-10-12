b07b7af5bae0df383d5592dd429716cb6131ba46
StanoevStefan <stefan.stanoev@student.upt.ro>
2023-10-12 14:45:39 +0300

    commit

--- src/Ex1/main.java
+++ src/Ex1/main.java
@@ -1,8 +1,42 @@
 package Ex1;
 
+import java.util.ArrayList;
+import java.util.List;
+
+class Punct {
+    private final int x;
+    private final int y;
+
+    public Punct(int x, int y) {
+        super();
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + "," + y + ")";
+    }
+}
+
 public class main {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        List<Punct> puncte = new ArrayList<Punct>();
+        puncte.add(new Punct(1, 2));
+        puncte.add(new Punct(3, 4));
+        puncte.add(new Punct(5, 6));
+
+        System.out.println("Lista contine punctele:");
+        for (Punct p : puncte) {
+            System.out.println(p);
+        }
+
+        puncte.remove(0);
+
+        System.out.println("Dupa stergerea primului element "
+                + "lista contine punctele:");
+        for (int i = 0; i < puncte.size(); i++)
+            System.out.println(puncte.get(i));
 
     }
 }
